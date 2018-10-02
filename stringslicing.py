c325dda50d3161c17a005ad777debcc7a9198d7d
Anshul104 <42971262+Anshul104@users.noreply.github.com>
2018-10-02 10:23:10 -0400

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,8 @@
+'''
+Created on Oct 2, 2018
+
+@author: aramavath22
+'''
+firstname = input('What is your first name:')
+lastname = input('What is your last name:')
+fullname = (firstname + lastname)
